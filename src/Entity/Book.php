24167c25871a4be4d065b3bdd357dbf26f8e134e
DjCtavia <djctavia@gmail.com>
2024-08-30 16:42:57 +0200

    feat(entity): add read group for serialization

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -6,6 +6,7 @@ use App\Repository\BookRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
@@ -16,9 +17,11 @@ class Book
     private ?string $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read'])]
     private ?string $isbn = null;
 
     /**
