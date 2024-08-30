e12569be80cf83a10e4ee5f3a812d9e0be086998
DjCtavia <djctavia@gmail.com>
2024-08-30 12:51:54 +0200

    feat(migration): initial migration

---
+++ migrations/Version20240830105042.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240830105042 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE book (id UUID NOT NULL, name VARCHAR(255) NOT NULL, isbn VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN book.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE library (id UUID NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN library.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE shelf (id UUID NOT NULL, library_id UUID NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_A5475BE3FE2541D7 ON shelf (library_id)');
+        $this->addSql('COMMENT ON COLUMN shelf.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN shelf.library_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE shelf_book (shelf_id UUID NOT NULL, book_id UUID NOT NULL, PRIMARY KEY(shelf_id, book_id))');
+        $this->addSql('CREATE INDEX IDX_431D356F7C12FBC0 ON shelf_book (shelf_id)');
+        $this->addSql('CREATE INDEX IDX_431D356F16A2B381 ON shelf_book (book_id)');
+        $this->addSql('COMMENT ON COLUMN shelf_book.shelf_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN shelf_book.book_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE user_book_borrow (id UUID NOT NULL, borrower_id UUID DEFAULT NULL, due_date_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_D5176AA811CE312B ON user_book_borrow (borrower_id)');
+        $this->addSql('COMMENT ON COLUMN user_book_borrow.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN user_book_borrow.borrower_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN user_book_borrow.due_date_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN user_book_borrow.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN user_book_borrow.updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.available_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.delivered_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE shelf ADD CONSTRAINT FK_A5475BE3FE2541D7 FOREIGN KEY (library_id) REFERENCES library (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE shelf_book ADD CONSTRAINT FK_431D356F7C12FBC0 FOREIGN KEY (shelf_id) REFERENCES shelf (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE shelf_book ADD CONSTRAINT FK_431D356F16A2B381 FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_book_borrow ADD CONSTRAINT FK_D5176AA811CE312B FOREIGN KEY (borrower_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE shelf DROP CONSTRAINT FK_A5475BE3FE2541D7');
+        $this->addSql('ALTER TABLE shelf_book DROP CONSTRAINT FK_431D356F7C12FBC0');
+        $this->addSql('ALTER TABLE shelf_book DROP CONSTRAINT FK_431D356F16A2B381');
+        $this->addSql('ALTER TABLE user_book_borrow DROP CONSTRAINT FK_D5176AA811CE312B');
+        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE library');
+        $this->addSql('DROP TABLE shelf');
+        $this->addSql('DROP TABLE shelf_book');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE user_book_borrow');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
